7feb7b600b03f52fa0ae2a3f0ab0ee0f4809d9f3
yellow013 <wk9988@gmail.com>
2020-06-25 18:18:51 +0800

    update hash

--- persistence/persistence-chronicle/src/main/java/io/mercury/persistence/chronicle/hash/AdjustableChronicleSet.java
+++ persistence/persistence-chronicle/src/main/java/io/mercury/persistence/chronicle/hash/AdjustableChronicleSet.java
@@ -2,7 +2,7 @@ package io.mercury.persistence.chronicle.hash;
 
 import net.openhft.chronicle.set.ChronicleSet;
 
-public class AdjustableChronicleSet<K> implements AdjustableChronicle<ChronicleSet<K>> {
+public final class AdjustableChronicleSet<K> implements AdjustableChronicle<ChronicleSet<K>> {
 
 	private ChronicleSet<K> entity;
 
