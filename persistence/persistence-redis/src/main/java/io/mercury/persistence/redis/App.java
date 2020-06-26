d2fafd903011c908009e49d81033a938dc9e9a66
yellow013 <wk9988@gmail.com>
2020-06-26 15:21:59 +0800

    update persistecne

--- persistence/persistence-redis/src/main/java/io/mercury/persistence/redis/App.java
+++ persistence/persistence-redis/src/main/java/io/mercury/persistence/redis/App.java
@@ -11,6 +11,7 @@ import io.lettuce.core.api.sync.RedisCommands;
 public class App {
 
 	public static void main(String[] args) {
+
 		RedisClient redisClient = RedisClient.create("redis://password@localhost:6379/0");
 		StatefulRedisConnection<String, String> connection = redisClient.connect();
 		RedisCommands<String, String> syncCommands = connection.sync();
