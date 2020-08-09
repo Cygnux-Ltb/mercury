17c00955b2c75f79dce22f924d855082e347c60b
yellow013 <wk9988@gmail.com>
2020-08-09 22:39:37 +0800

    update commons

--- commons/commons-core/src/main/java/io/mercury/common/file/FileChannelWriter.java
+++ commons/commons-core/src/main/java/io/mercury/common/file/FileChannelWriter.java
@@ -5,13 +5,16 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 import javax.annotation.Nonnull;
 
 import org.apache.commons.collections4.CollectionUtils;
 
+import io.mercury.common.character.Charsets;
 import io.mercury.common.character.Separator;
 import io.mercury.common.sys.SysProperties;
 
@@ -30,61 +33,113 @@ public final class FileChannelWriter {
 	 * @param data   : Written data
 	 * @param target : Written target file
 	 * @return
+	 * @throws NullPointerException
 	 * @throws IOException
 	 */
-	public static final File write(List<String> data, @Nonnull final File target) throws IOException {
-		return write(data, target, 8192);
+	public static final File write(List<String> data, @Nonnull final File target)
+			throws NullPointerException, IOException {
+		return write(data, Charsets.UTF8, target, 8192, true);
+	}
+
+	/**
+	 * 
+	 * @param data   : Written data
+	 * @param target : Written target file
+	 * @param append : Is append file end
+	 * @return
+	 * @throws NullPointerException
+	 * @throws IOException
+	 */
+	public static final File write(List<String> data, @Nonnull File target, boolean append)
+			throws NullPointerException, IOException {
+		return write(data, Charsets.UTF8, target, 8192, append);
 	}
 
 	/**
 	 * 
 	 * @param data     : Written data
+	 * @param charset  : Value charset
 	 * @param target   : Written target file
 	 * @param capacity : Buffer capacity
+	 * @param append   : Is append file end
 	 * @return
+	 * @throws NullPointerException
 	 * @throws IOException
 	 */
-	public static final File write(List<String> data, @Nonnull final File target, final int capacity)
-			throws NullPointerException, IOException {
+	public static final File write(List<String> data, @Nonnull Charset charset, @Nonnull File target, int capacity,
+			boolean append) throws NullPointerException, IOException {
+		return write(data, str -> str.getBytes(charset), target, capacity, append);
+	}
+
+	/**
+	 * 
+	 * @param <T>
+	 * @param data          : Written data
+	 * @param serialization : Serialization function
+	 * @param target        : Written target file
+	 * @param capacity      : Buffer capacity
+	 * @param append        : Is append file end
+	 * @return
+	 * @throws NullPointerException
+	 * @throws IOException
+	 */
+	public static final <T> File write(List<T> data, @Nonnull Function<T, byte[]> serialization, @Nonnull File target,
+			int capacity, boolean append) throws NullPointerException, IOException {
 		if (target == null)
 			throw new NullPointerException("target file must not be null.");
+		if (capacity <= 0)
+			capacity = 4096;
 		File parentFile = target.getParentFile();
 		if (!parentFile.exists())
 			parentFile.mkdirs();
 		if (!target.exists())
 			target.createNewFile();
 		if (CollectionUtils.isNotEmpty(data)) {
-			try (RandomAccessFile rafile = new RandomAccessFile(target, "rw");
-					FileChannel channel = rafile.getChannel()) {
-				// Allocate [capacity] direct buffer
-				ByteBuffer buffer = ByteBuffer.allocateDirect(capacity);
-				for (int i = 0; i < data.size(); i++) {
-					byte[] bytes = data.get(i).getBytes();
-					if (bytes.length < capacity) {
-						// [bytes#length] < [capacity], Only need to write once
-						flipAndChannelWrite(buffer.put(bytes), channel);
-					} else {
-						// Count writes
-						int count = bytes.length / capacity;
-						int offset = 0;
-						for (int r = 0; r < count; r++) {
-							// Write from the last [offset], Write length is buffer [capacity]
-							flipAndChannelWrite(buffer.put(bytes, offset = r * capacity, capacity), channel);
+			try (RandomAccessFile rafile = new RandomAccessFile(target, "rw")) {
+				if (append) {
+					// Seek to end
+					rafile.seek(rafile.length());
+				}
+				try (FileChannel channel = rafile.getChannel()) {
+					// Allocate [capacity] direct buffer
+					ByteBuffer buffer = ByteBuffer.allocateDirect(capacity);
+					for (int i = 0; i < data.size(); i++) {
+						byte[] bytes = serialization.apply(data.get(i));
+						if (bytes == null || bytes.length == 0) {
+							continue;
 						}
-						// Remaining data
-						int remaining = bytes.length % capacity;
-						if (remaining > 0) {
-							// Write from the last [offset], Write length is remaining
-							flipAndChannelWrite(buffer.put(bytes, offset += capacity, remaining), channel);
+						if (bytes.length < capacity) {
+							// [bytes#length] < [capacity], Only need to write once
+							flipAndChannelWrite(buffer.put(bytes), channel);
+						} else {
+							// Count writes
+							int count = bytes.length / capacity;
+							int offset = 0;
+							for (int r = 0; r < count; r++) {
+								// Write from the last [offset], Write length is buffer [capacity]
+								flipAndChannelWrite(buffer.put(bytes, offset = r * capacity, capacity), channel);
+							}
+							// Remaining data
+							int remaining = bytes.length % capacity;
+							if (remaining > 0) {
+								// Write from the last [offset], Write length is remaining
+								flipAndChannelWrite(buffer.put(bytes, offset += capacity, remaining), channel);
+							}
 						}
 					}
+					channel.force(true);
 				}
-				channel.force(true);
 			}
 		}
 		return target;
 	}
 
+	/**
+	 * 
+	 * @param buffer
+	 * @param channel
+	 * @throws IOException
+	 */
 	private static final void flipAndChannelWrite(ByteBuffer buffer, FileChannel channel) throws IOException {
 		// Flip buffer to output
 		buffer.flip();
@@ -112,7 +167,7 @@ public final class FileChannelWriter {
 		}
 
 		try {
-			write(lines, new File(SysProperties.JAVA_IO_TMPDIR_FILE, "test.csv"), 2048);
+			write(lines, Charsets.UTF8, new File(SysProperties.JAVA_IO_TMPDIR_FILE, "test.csv"), 2048, true);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
