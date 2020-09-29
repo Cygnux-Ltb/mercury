80284886dfa509235c742338f5b69b7319fe8fef
yellow013 <wk9988@gmail.com>
2020-09-29 22:39:28 +0800

    update cassander

--- persistence/persistence-cassandra/src/main/java/io/mercury/persistence/cassandra/App.java
+++ persistence/persistence-cassandra/src/main/java/io/mercury/persistence/cassandra/App.java
@@ -1,13 +1,9 @@
 package io.mercury.persistence.cassandra;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
 }
