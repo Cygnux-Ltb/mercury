5519aa945a8ff58995c16a190b96d5e088e00c10
yellow013 <wk9988@gmail.com>
2020-10-05 00:00:10 +0800

    update json

--- persistence/serialization-protobuf/src/main/java/io/mercury/serialization/protobuf/App.java
+++ persistence/serialization-protobuf/src/main/java/io/mercury/serialization/protobuf/App.java
@@ -4,10 +4,10 @@ package io.mercury.serialization.protobuf;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
 }
