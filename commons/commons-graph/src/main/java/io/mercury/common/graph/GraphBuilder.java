94a504971c37867aee9f777fd74a5d496a34879e
yellow013 <wk9988@gmail.com>
2020-11-01 23:36:35 +0800

    update graph

--- commons/commons-graph/src/main/java/io/mercury/common/graph/GraphBuilder.java
+++ commons/commons-graph/src/main/java/io/mercury/common/graph/GraphBuilder.java
@@ -12,47 +12,86 @@ public final class GraphBuilder {
 
 	private static final Logger log = CommonLoggerFactory.getLogger(GraphBuilder.class);
 
-	/**
-	 * 有向图
-	 * 
-	 * @param <V>
-	 * @param vClass
-	 * @return
-	 */
-	public static <V> Graph<V, Edge> directed(Class<V> vClass) {
-		GraphTypeBuilder<V, Edge> builder = GraphTypeBuilder
-				.directed()
-				.vertexClass(vClass)
-				.edgeSupplier(Edge::new);
+	public static <V, E extends Edge> Graph<V, E> directed(Class<V> vertexClass, Class<E> edgeClass) {
+		return directed(vertexClass, edgeClass, defaultOptions);
+	}
+
+	public static <V, E extends Edge> Graph<V, E> directed(Class<V> vertexClass, Class<E> edgeClass,
+			GraphOptions options) {
+		GraphTypeBuilder<V, E> builder = GraphTypeBuilder.directed().vertexClass(vertexClass).edgeClass(edgeClass)
+				.allowingMultipleEdges(options.isAllowingMultipleEdges())
+				.allowingSelfLoops(options.isAllowingSelfLoops()).weighted(options.isWeighted());
 		log.info("GraphBuilder -> {}", builder.buildType());
 		return builder.buildGraph();
 	}
 
-	/**
-	 * 有向图
-	 * 
-	 * @param <V>
-	 * @param <E>
-	 * @param vClass
-	 * @param edgeSupplier
-	 * @return
-	 */
-	public static <V, E extends Edge> Graph<V, E> directed(Class<V> vClass, Supplier<E> edgeSupplier) {
-		GraphTypeBuilder<V, E> builder = GraphTypeBuilder
-				.directed()
-				.vertexClass(vClass)
-				.edgeSupplier(edgeSupplier);
+	public static <V> Graph<V, Edge> directed(Class<V> vertexClass) {
+		return directed(vertexClass, defaultOptions);
+	}
+
+	public static <V> Graph<V, Edge> directed(Class<V> vertexClass, GraphOptions options) {
+		return directed(vertexClass, Edge::new, options);
+	}
+
+	public static <V, E extends Edge> Graph<V, E> directed(Class<V> vertexClass, Supplier<E> edgeSupplier) {
+		return directed(vertexClass, edgeSupplier, defaultOptions);
+	}
+
+	public static <V, E extends Edge> Graph<V, E> directed(Class<V> vertexClass, Supplier<E> edgeSupplier,
+			GraphOptions options) {
+		GraphTypeBuilder<V, E> builder = GraphTypeBuilder.directed().vertexClass(vertexClass).edgeSupplier(edgeSupplier)
+				.allowingMultipleEdges(options.isAllowingMultipleEdges())
+				.allowingSelfLoops(options.isAllowingSelfLoops()).weighted(options.isWeighted());
 		log.info("GraphBuilder -> {}", builder.buildType());
 		return builder.buildGraph();
 	}
+
+	private final static GraphOptions defaultOptions = GraphOptions.defaultOptions();
 
 	/**
 	 * 
 	 * @author yellow013
 	 *
 	 */
-	public static class Builder {
+	public static class GraphOptions {
 
+		private boolean allowingMultipleEdges = false;
+		private boolean allowingSelfLoops = false;
+		private boolean weighted = false;
+
+		private GraphOptions() {
+		}
+
+		public static final GraphOptions defaultOptions() {
+			return new GraphOptions();
+		}
+
+		public boolean isAllowingMultipleEdges() {
+			return allowingMultipleEdges;
+		}
+
+		public boolean isAllowingSelfLoops() {
+			return allowingSelfLoops;
+		}
+
+		public boolean isWeighted() {
+			return weighted;
+		}
+
+		public GraphOptions setAllowingMultipleEdges(boolean allowingMultipleEdges) {
+			this.allowingMultipleEdges = allowingMultipleEdges;
+			return this;
+		}
+
+		public GraphOptions setAllowingSelfLoops(boolean allowingSelfLoops) {
+			this.allowingSelfLoops = allowingSelfLoops;
+			return this;
+		}
+
+		public GraphOptions setWeighted(boolean weighted) {
+			this.weighted = weighted;
+			return this;
+		}
 	}
 
 }
