f77c80453b4e19e0c21ed3b372d62e8bee4573bf
yellow013 <wk9988@gmail.com>
2020-11-30 17:10:40 +0800

    update persistence

---
+++ persistence/serialization-protostuff/src/main/java/io/mercury/serialization/protostuff/App.java
@@ -0,0 +1,13 @@
+package io.mercury.serialization.protostuff;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
