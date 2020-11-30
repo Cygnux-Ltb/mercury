03fb91d23f26f9850d342a6ef6a2c0628e91f142
yellow013 <wk9988@gmail.com>
2020-11-30 11:44:58 +0800

    update disruptor

--- commons/commons-concurrent/src/main/java/io/mercury/common/concurrent/disruptor/SpscQueueWithSupplier.java
+++ commons/commons-concurrent/src/main/java/io/mercury/common/concurrent/disruptor/SpscQueueWithSupplier.java
@@ -11,12 +11,12 @@ import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.dsl.ProducerType;
 
 import io.mercury.common.collections.Capacity;
-import io.mercury.common.concurrent.queue.SCQueue;
+import io.mercury.common.concurrent.queue.base.ScQueue;
 import io.mercury.common.functional.Processor;
 import io.mercury.common.log.CommonLoggerFactory;
 import io.mercury.common.thread.Threads;
 
-public class SpscQueueWithSupplier<T> extends SCQueue<T> {
+public class SpscQueueWithSupplier<T> extends ScQueue<T> {
 
 	private static final Logger log = CommonLoggerFactory.getLogger(SpscQueueWithSupplier.class);
 
@@ -122,6 +122,11 @@ public class SpscQueueWithSupplier<T> extends SCQueue<T> {
 
 		Threads.sleep(10000);
 
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
 	}
 
 }
