7cc69da22e6d4b4d14299ecc9b41a5e44aec90c7
yellow013 <wk9988@gmail.com>
2020-11-30 11:44:25 +0800

    update queue file

---
+++ commons/commons-concurrent/src/main/java/io/mercury/common/concurrent/queue/QueueStyle.java
@@ -0,0 +1,7 @@
+package io.mercury.common.concurrent.queue;
+
+public enum QueueStyle {
+
+	SPSC, SPMC, MPSC, MPMC
+
+}
---
+++ commons/commons-concurrent/src/main/java/io/mercury/common/concurrent/queue/WaitingStrategy.java
@@ -0,0 +1,7 @@
+package io.mercury.common.concurrent.queue;
+
+public enum WaitingStrategy {
+
+	SpinWaiting, SleepWaiting, Blocking
+
+}
