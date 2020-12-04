297b521229eab5788f8477f61602b5d5337467b9
yellow013 <wk9988@gmail.com>
2020-12-04 20:36:00 +0800

    update arangodb

--- libraries/library-arangodb/src/test/java/io/mercury/persistence/arangodb/AppTest.java
+++
@@ -1,5 +0,0 @@
-package io.mercury.persistence.arangodb;
-
-public class AppTest {
-
-}
---
+++ libraries/library-arangodb/src/test/java/io/mercury/persistence/arangodb/ArangoDemoTest.java
@@ -0,0 +1,11 @@
+package io.mercury.persistence.arangodb;
+
+import org.junit.Test;
+
+public class ArangoDemoTest {
+
+	@Test
+	public void test() {
+	}
+
+}
