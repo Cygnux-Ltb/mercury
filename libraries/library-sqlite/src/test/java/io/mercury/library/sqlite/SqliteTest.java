a5faa2b5132a66b7179bf16d3eab2b7690a3d296
yellow013 <wk9988@gmail.com>
2020-12-04 20:36:23 +0800

    update sqlite

--- libraries/library-sqlite/src/test/java/io/mercury/library/sqlite/SqliteTest.java
+++ libraries/library-sqlite/src/test/java/io/mercury/library/sqlite/SqliteTest.java
@@ -1,35 +1,136 @@
 package io.mercury.library.sqlite;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
+public class SqliteTest {
+
+	private Connection connection;
+	private Statement statement;
 
 	/**
-	 * Create the test case
-	 *
-	 * @param testName name of the test case
+	 * 连接到一个现有的数据库。如果数据库不存在， 那么它就会被创建，最后将返回一个数据库对象。
 	 */
-	public AppTest(String testName) {
-		super(testName);
+	@Before
+	public void before() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			connection = DriverManager.getConnection("jdbc:sqlite:C:\\db\\test.db");
+			System.out.println("Opened database successfully");
+			statement = connection.createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	/**
-	 * @return the suite of tests being tested
-	 */
-	public static Test suite() {
-		return new TestSuite(AppTest.class);
+	@After
+	public void after() {
+		try {
+			statement.close();
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
-	/**
-	 * Rigourous Test :-)
-	 */
-	public void testApp() {
-		assertTrue(true);
+	@Test
+	public void createTable() throws SQLException {
+		String sql = "CREATE TABLE COMPANY " + "(ID INT PRIMARY KEY NOT NULL," + " NAME TEXT NOT NULL,"
+				+ " AGE INT NOT NULL," + " ADDRESS CHAR(50)," + " SALARY REAL)";
+		statement.executeUpdate(sql);
+	}
+
+	@Test
+	public void insert() throws SQLException {
+		connection.setAutoCommit(false);
+		StringBuffer sb = new StringBuffer();
+		sb.append("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+				+ "VALUES (1, 'Paul', 32, 'California', 20000.00 );\n");
+		sb.append(
+				"INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " + "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );\n");
+		sb.append("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+				+ "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );\n");
+		sb.append("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+				+ "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );");
+		statement.executeUpdate(sb.toString());
+		connection.commit();
+	}
+
+	@Test
+	public void select() throws SQLException {
+		try (ResultSet rs = statement.executeQuery("SELECT * FROM COMPANY;")) {
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				int age = rs.getInt("age");
+				String address = rs.getString("address");
+				float salary = rs.getFloat("salary");
+				System.out.println("ID = " + id);
+				System.out.println("NAME = " + name);
+				System.out.println("AGE = " + age);
+				System.out.println("ADDRESS = " + address);
+				System.out.println("SALARY = " + salary);
+				System.out.println("--------");
+			}
+		}
+	}
+
+	@Test
+	public void update() throws SQLException {
+		connection.setAutoCommit(false);
+		String sql = "UPDATE COMPANY set SALARY = 25000.00 where ID=1;";
+		statement.executeUpdate(sql);
+		connection.commit();
+
+		ResultSet rs = statement.executeQuery("SELECT * FROM COMPANY;");
+		while (rs.next()) {
+			int id = rs.getInt("id");
+			String name = rs.getString("name");
+			int age = rs.getInt("age");
+			String address = rs.getString("address");
+			float salary = rs.getFloat("salary");
+			System.out.println("ID = " + id);
+			System.out.println("NAME = " + name);
+			System.out.println("AGE = " + age);
+			System.out.println("ADDRESS = " + address);
+			System.out.println("SALARY = " + salary);
+			System.out.println("--------");
+		}
+		rs.close();
+	}
+
+	@Test
+	public void delete() throws SQLException {
+		connection.setAutoCommit(false);
+		String sql = "DELETE from COMPANY where ID=2;";
+		statement.executeUpdate(sql);
+		connection.commit();
+
+		ResultSet rs = statement.executeQuery("SELECT * FROM COMPANY;");
+		while (rs.next()) {
+			int id = rs.getInt("id");
+			String name = rs.getString("name");
+			int age = rs.getInt("age");
+			String address = rs.getString("address");
+			float salary = rs.getFloat("salary");
+			System.out.println("ID = " + id);
+			System.out.println("NAME = " + name);
+			System.out.println("AGE = " + age);
+			System.out.println("ADDRESS = " + address);
+			System.out.println("SALARY = " + salary);
+			System.out.println("--------");
+		}
+		rs.close();
 	}
 
 }
