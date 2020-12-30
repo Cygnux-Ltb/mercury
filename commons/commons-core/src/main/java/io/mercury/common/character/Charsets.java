65e85d6115286de21c347e9a0449eb56eba55cf8
yellow013 <wk9988@gmail.com>
2020-12-30 15:16:27 +0800

    update character

--- commons/commons-core/src/main/java/io/mercury/common/character/Charsets.java
+++ commons/commons-core/src/main/java/io/mercury/common/character/Charsets.java
@@ -1,21 +1,23 @@
 package io.mercury.common.character;
 
+import static java.nio.charset.Charset.forName;
+
 import java.nio.charset.Charset;
 
 public interface Charsets {
 
-	Charset ASCII = Charset.forName("ASCII");
+	Charset ASCII = forName("ASCII");
 
-	Charset ISO_8859_1 = Charset.forName("ISO-8859-1");
+	Charset ISO_8859_1 = forName("ISO-8859-1");
 
-	Charset UTF8 = Charset.forName("UTF-8");
+	Charset UTF8 = forName("UTF-8");
 
-	Charset UTF16 = Charset.forName("UTF-16");
+	Charset UTF16 = forName("UTF-16");
 
-	Charset UTF32 = Charset.forName("UTF-32");
+	Charset UTF32 = forName("UTF-32");
 
-	Charset GBK = Charset.forName("GBK");
+	Charset GBK = forName("GBK");
 
-	Charset GB2312 = Charset.forName("GB2312");
+	Charset GB2312 = forName("GB2312");
 
 }
