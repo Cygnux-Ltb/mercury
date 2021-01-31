b8469917bcfce5232f7a1dda6a86a56ab40ac78c
yellow013 <wk9988@gmail.com>
2021-01-31 23:54:37 +0800

    rocksdb update

--- persistence/persistence-rocksdb/src/main/java/io/mercury/persistence/rocksdb/RocksMapKeeper.java
+++ persistence/persistence-rocksdb/src/main/java/io/mercury/persistence/rocksdb/RocksMapKeeper.java
@@ -1,11 +1,11 @@
 package io.mercury.persistence.rocksdb;
 
-import io.mercury.common.collections.keeper.KeeperBaseImpl;
+import io.mercury.common.collections.keeper.AbstractKeeper;
 import io.mercury.persistence.rocksdb.map.RocksMap;
 import io.mercury.persistence.rocksdb.map.kv.RocksKey;
 import io.mercury.persistence.rocksdb.map.kv.RocksValue;
 
-public class RocksMapKeeper<K extends RocksKey, V extends RocksValue> extends KeeperBaseImpl<String, RocksMap<K, V>> {
+public class RocksMapKeeper<K extends RocksKey, V extends RocksValue> extends AbstractKeeper<String, RocksMap<K, V>> {
 
 	@Override
 	protected RocksMap<K, V> createWithKey(String k) {
