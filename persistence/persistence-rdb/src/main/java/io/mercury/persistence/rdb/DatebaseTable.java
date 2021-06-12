3dea1568244231c7fa4aa3a337ed5b8f7ae6ba3d
yellow013 <wk9988@gmail.com>
2021-06-12 23:48:43 +0800

    update rdb persistence

---
+++ persistence/persistence-rdb/src/main/java/io/mercury/persistence/rdb/DatebaseTable.java
@@ -0,0 +1,22 @@
+package io.mercury.persistence.rdb;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface DatebaseTable {
+
+	boolean ignoreExisted() default true;
+
+	String tableName();
+
+	String createTableStatement();
+
+}
---
+++ persistence/persistence-rdb/src/main/java/io/mercury/persistence/rdb/DatebaseTableScanner.java
@@ -0,0 +1,17 @@
+package io.mercury.persistence.rdb;
+
+import java.sql.Connection;
+
+public final class DatebaseTableScanner {
+
+	private DatebaseTableScanner() {
+	}
+	
+	public static void scanPackage(String packageName, Connection connection) {
+		
+		
+		
+		
+	}
+
+}
