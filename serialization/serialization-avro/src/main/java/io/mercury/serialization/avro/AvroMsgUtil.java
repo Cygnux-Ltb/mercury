d06d0c2addf89e6e45b9e43c117e0611354d3817
yellow013 <wk9988@gmail.com>
2021-08-08 19:31:09 +0800

    update serialization

--- serialization/serialization-avro/src/main/java/io/mercury/serialization/avro/AvroMsgUtil.java
+++ serialization/serialization-avro/src/main/java/io/mercury/serialization/avro/AvroMsgUtil.java
@@ -9,12 +9,10 @@ import io.mercury.serialization.avro.msg.AvroTextMsg;
 public class AvroMsgUtil {
 
 	public static boolean hasEpoch(@Nonnull Collection<AvroTextMsg> msgs) {
-
 		return false;
 	}
 
 	public static boolean hasSequence(@Nonnull Collection<AvroTextMsg> msgs) {
-
 		return false;
 	}
 
