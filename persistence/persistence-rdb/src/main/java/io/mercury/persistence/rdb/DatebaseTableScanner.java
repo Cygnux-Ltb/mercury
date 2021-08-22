ec7fe78e2230333eeb323330db49e6435dfc56bd
yellow013 <wk9988@gmail.com>
2021-08-22 17:27:20 +0800

    update persistence

--- persistence/persistence-rdb/src/main/java/io/mercury/persistence/rdb/DatebaseTableScanner.java
+++ persistence/persistence-rdb/src/main/java/io/mercury/persistence/rdb/DatebaseTableScanner.java
@@ -3,17 +3,12 @@ package io.mercury.persistence.rdb;
 import java.sql.Connection;
 
 public final class DatebaseTableScanner {
-	
-	
 
 	private DatebaseTableScanner() {
 	}
-	
+
 	public static void scanPackage(String packageName, Connection connection) {
-		
-		
-		
-		
+
 	}
 
 }
