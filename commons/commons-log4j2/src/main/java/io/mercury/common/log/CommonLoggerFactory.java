464781e9503213fb7f2ab5503766bbf291053cd3
yellow013 <wk9988@gmail.com>
2021-11-10 21:50:02 +0800

    update log4j2

--- commons/commons-log4j2/src/main/java/io/mercury/common/log/CommonLoggerFactory.java
+++ commons/commons-log4j2/src/main/java/io/mercury/common/log/CommonLoggerFactory.java
@@ -1,7 +1,7 @@
 package io.mercury.common.log;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import static java.time.LocalDateTime.now;
+import static java.time.format.DateTimeFormatter.ofPattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,8 +17,7 @@ public final class CommonLoggerFactory {
 
 	public static final String DefaultFolder = "default";
 
-	public static final String DefaultFileName = "runtime-"
-			+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
+	public static final String DefaultFileName = "runtime-" + now().format(ofPattern("yyyyMMdd-HHmmss"));
 
 	public static final LogLevel DefaultLevel = LogLevel.ERROR;
 
