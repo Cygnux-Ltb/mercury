b750163fd5775c062fa5631b533f878c2808f2ff
yellow013 <wk9988@gmail.com>
2021-12-24 23:29:34 +0800

    update config

--- commons/commons-config/src/main/java/io/mercury/common/config/ConfigDelegate.java
+++ commons/commons-config/src/main/java/io/mercury/common/config/ConfigDelegate.java
@@ -1,8 +1,11 @@
 package io.mercury.common.config;
 
+import java.util.function.Predicate;
+
 import javax.annotation.Nonnull;
 
 import com.typesafe.config.Config;
+import com.typesafe.config.ConfigException;
 
 import io.mercury.common.functional.Functions;
 import io.mercury.common.lang.Assertor;
@@ -17,17 +20,38 @@ public final class ConfigDelegate<O extends ConfigOption> {
 	}
 
 	/**
+	 * 是否存在此项配置, 并且配置不为空
 	 * 
 	 * @param option
 	 * @return
 	 */
-	public boolean getBoolean(@Nonnull O option) {
-		return Functions.getOrThrows(() -> config.hasPath(option.getConfigName()),
-				() -> config.getBoolean(option.getConfigName()),
-				"Missing [" + option.getConfigName() + "] config option");
+	public boolean hasOption(@Nonnull O option) {
+		return config.hasPath(option.getConfigName());
 	}
 
 	/**
+	 * 是否存在此项配置, 或此项配置为空
+	 * 
+	 * @param option
+	 * @return
+	 */
+	public boolean hasOptionOrNull(@Nonnull O option) {
+		return config.hasPathOrNull(option.getConfigName());
+	}
+
+	/**
+	 * 获取[boolean]配置值, 如果未配置, 默认值[false]
+	 * 
+	 * @param option
+	 * @param defaultVal
+	 * @return
+	 */
+	public boolean getBoolean(@Nonnull O option) {
+		return getBoolean(option, false);
+	}
+
+	/**
+	 * 获取[boolean]配置值, 如果未配置, 使用指定默认值
 	 * 
 	 * @param option
 	 * @param defaultVal
@@ -39,16 +63,18 @@ public final class ConfigDelegate<O extends ConfigOption> {
 	}
 
 	/**
+	 * 获取[int]配置值, 如果未配置, 默认值[0]
 	 * 
 	 * @param option
+	 * @param defaultVal
 	 * @return
 	 */
 	public int getInt(@Nonnull O option) {
-		return Functions.getOrThrows(() -> config.hasPath(option.getConfigName()),
-				() -> config.getInt(option.getConfigName()), "Missing [" + option.getConfigName() + "] config option");
+		return getInt(option, 0);
 	}
 
 	/**
+	 * 获取[int]配置值, 如果未配置, 使用指定默认值
 	 * 
 	 * @param option
 	 * @param defaultVal
@@ -60,16 +86,18 @@ public final class ConfigDelegate<O extends ConfigOption> {
 	}
 
 	/**
+	 * 获取[long]配置值, 如果未配置, 默认值[0L]
 	 * 
 	 * @param option
+	 * @param defaultVal
 	 * @return
 	 */
 	public long getLong(@Nonnull O option) {
-		return Functions.getOrThrows(() -> config.hasPath(option.getConfigName()),
-				() -> config.getLong(option.getConfigName()), "Missing [" + option.getConfigName() + "] config option");
+		return getLong(option, 0L);
 	}
 
 	/**
+	 * 获取[long]配置值, 如果未配置, 使用指定默认值
 	 * 
 	 * @param option
 	 * @param defaultVal
@@ -81,17 +109,18 @@ public final class ConfigDelegate<O extends ConfigOption> {
 	}
 
 	/**
+	 * 获取[double]配置值, 如果未配置, 默认值[0.0D]
 	 * 
 	 * @param option
+	 * @param defaultVal
 	 * @return
 	 */
 	public double getDouble(@Nonnull O option) {
-		return Functions.getOrThrows(() -> config.hasPath(option.getConfigName()),
-				() -> config.getDouble(option.getConfigName()),
-				"Missing [" + option.getConfigName() + "] config option");
+		return getDouble(option, 0.0D);
 	}
 
 	/**
+	 * 获取[double]配置值, 如果未配置, 使用指定默认值
 	 * 
 	 * @param option
 	 * @param defaultVal
@@ -103,17 +132,18 @@ public final class ConfigDelegate<O extends ConfigOption> {
 	}
 
 	/**
+	 * 获取[String]配置值, 如果未配置, 默认值[""]
 	 * 
 	 * @param option
+	 * @param defaultVal
 	 * @return
 	 */
 	public String getString(@Nonnull O option) {
-		return Functions.getOrThrows(() -> config.hasPath(option.getConfigName()),
-				() -> config.getString(option.getConfigName()),
-				"Missing [" + option.getConfigName() + "] config option");
+		return getString(option, "");
 	}
 
 	/**
+	 * 获取[String]配置值, 如果未配置, 使用指定默认值
 	 * 
 	 * @param option
 	 * @param defaultVal
@@ -122,6 +152,93 @@ public final class ConfigDelegate<O extends ConfigOption> {
 	public String getString(@Nonnull O option, @Nonnull String defaultVal) {
 		return Functions.getOrDefault(() -> config.hasPath(option.getConfigName()),
 				() -> config.getString(option.getConfigName()), defaultVal);
+	}
+
+	/**
+	 * 
+	 * @param option
+	 * @return
+	 * @throws ConfigException.Missing
+	 */
+	public boolean getBooleanOrThrows(@Nonnull O option) throws ConfigException.Missing {
+		return Functions.getOrThrows(() -> config.hasPath(option.getConfigName()),
+				() -> config.getBoolean(option.getConfigName()), new ConfigException.Missing(option.getConfigName()));
+	}
+
+	/**
+	 * 
+	 * @param option
+	 * @return
+	 * @throws ConfigException.Missing
+	 */
+	public int getIntOrThrows(@Nonnull O option) throws ConfigException.Missing {
+		return Functions.getOrThrows(() -> config.hasPath(option.getConfigName()),
+				() -> config.getInt(option.getConfigName()), new ConfigException.Missing(option.getConfigName()));
+	}
+
+	/**
+	 * 
+	 * @param option
+	 * @return
+	 * @throws ConfigException.Missing
+	 */
+	public long getLongOrThrows(@Nonnull O option) throws ConfigException.Missing {
+		return Functions.getOrThrows(() -> config.hasPath(option.getConfigName()),
+				() -> config.getLong(option.getConfigName()), new ConfigException.Missing(option.getConfigName()));
+	}
+
+	/**
+	 * 
+	 * @param option
+	 * @return
+	 * @throws ConfigException.Missing
+	 */
+	public double getDoubleOrThrows(@Nonnull O option) throws ConfigException.Missing {
+		return Functions.getOrThrows(() -> config.hasPath(option.getConfigName()),
+				() -> config.getDouble(option.getConfigName()), new ConfigException.Missing(option.getConfigName()));
+	}
+
+	/**
+	 * 
+	 * @param option
+	 * @return
+	 * @throws ConfigException.Missing
+	 */
+	public String getStringOrThrows(@Nonnull O option) throws ConfigException.Missing {
+		return Functions.getOrThrows(() -> config.hasPath(option.getConfigName()),
+				() -> config.getString(option.getConfigName()), new ConfigException.Missing(option.getConfigName()));
+	}
+
+	/**
+	 * 
+	 * @param option
+	 * @param predicate
+	 * @return
+	 * @throws ConfigException.Missing
+	 * @throws ConfigException.BadValue
+	 */
+	public String getStringOrThrows(@Nonnull O option, Predicate<String> predicate)
+			throws ConfigException.Missing, ConfigException.BadValue {
+		return getStringOrThrows(option, predicate,
+				new IllegalArgumentException("Illegal argument -> " + option.getConfigName()));
+	}
+
+	/**
+	 * 
+	 * @param option
+	 * @param predicate
+	 * @param exception
+	 * @return
+	 * @throws ConfigException.Missing
+	 * @throws ConfigException.BadValue
+	 */
+	public String getStringOrThrows(@Nonnull O option, Predicate<String> predicate, Exception exception)
+			throws ConfigException.Missing, ConfigException.BadValue {
+		String value = getStringOrThrows(option);
+		if (predicate.test(value))
+			return value;
+		else
+			throw new ConfigException.BadValue(option.getConfigName(), value, exception);
 	}
 
 }
