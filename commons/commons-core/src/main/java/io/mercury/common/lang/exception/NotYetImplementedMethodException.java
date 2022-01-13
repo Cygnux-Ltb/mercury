64eef28fa19f361b2f4f51cb2703e32cce33e3f5
yellow013 <wk9988@gmail.com>
2022-01-14 01:04:45 +0800

    update exception

---
+++ commons/commons-core/src/main/java/io/mercury/common/lang/exception/NotYetImplementedMethodException.java
@@ -0,0 +1,14 @@
+package io.mercury.common.lang.exception;
+
+public final class NotYetImplementedMethodException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 947213334800336824L;
+
+	public NotYetImplementedMethodException(String msg) {
+		super(msg);
+	}
+
+}
