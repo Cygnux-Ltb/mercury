6d042f66a1921e7ebec19d4ddd67976415946e2c
yellow013 <wk9988@gmail.com>
2022-01-18 21:33:10 +0800

    update transport

--- transport/transport-netty/src/main/java/io/mercury/transport/netty/NettySender.java
+++ transport/transport-netty/src/main/java/io/mercury/transport/netty/NettySender.java
@@ -3,7 +3,7 @@ package io.mercury.transport.netty;
 import org.slf4j.Logger;
 
 import io.mercury.common.log.Log4j2LoggerFactory;
-import io.mercury.common.sequence.LinearSequence;
+import io.mercury.common.sequence.SysNanoSequence;
 import io.mercury.transport.api.Sender;
 import io.mercury.transport.api.TransportComponent;
 import io.netty.buffer.ByteBuf;
@@ -49,12 +49,12 @@ public class NettySender extends TransportComponent implements Sender<byte[]> {
 
 	@Override
 	public void sent(byte[] msg) {
-		log.debug(LinearSequence.getMicros() + " call sender send -> data length : " + msg.length);
+		log.debug(SysNanoSequence.getMicros() + " call sender send -> data length : " + msg.length);
 		ByteBuf byteBuf = context.alloc().buffer(msg.length);
 		byteBuf.writeBytes(msg);
 		ChannelFuture writeAndFlush = context.writeAndFlush(byteBuf.retain());
 		writeAndFlush.addListener(future -> {
-			log.debug("{} call sender send operation complete -> data length : {}", LinearSequence.getMicros(),
+			log.debug("{} call sender send operation complete -> data length : {}", SysNanoSequence.getMicros(),
 					byteBuf.writerIndex());
 			byteBuf.clear();
 			byteBuf.release();
