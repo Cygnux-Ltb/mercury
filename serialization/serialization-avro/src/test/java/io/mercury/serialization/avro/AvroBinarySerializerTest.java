bb42d54341965e79e6bbfc1289e00e9cba1295e3
Yellow013 <wk9988@gmail.com>
2022-10-26 15:38:01 +0900

    update ser

--- serialization/serialization-avro/src/test/java/io/mercury/serialization/avro/AvroBinarySerializerTest.java
+++ serialization/serialization-avro/src/test/java/io/mercury/serialization/avro/AvroBinarySerializerTest.java
@@ -1,8 +1,8 @@
 package io.mercury.serialization.avro;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.fail;
 
 public class AvroBinarySerializerTest {
 
