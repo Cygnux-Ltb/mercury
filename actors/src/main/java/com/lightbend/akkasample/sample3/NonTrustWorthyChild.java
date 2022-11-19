5c8cf84e31f168482a11989cdd685297ac5873c3
Yellow013 <wk9988@gmail.com>
2022-11-19 22:17:02 +0900

    update actor

--- actors/src/main/java/com/lightbend/akkasample/sample3/NonTrustWorthyChild.java
+++ actors/src/main/java/com/lightbend/akkasample/sample3/NonTrustWorthyChild.java
@@ -5,31 +5,32 @@ import akka.actor.Props;
 
 public class NonTrustWorthyChild extends AbstractLoggingActor {
 
-	public static class Command {
-	}
+    private long messages = 0L;
 
-	private long messages = 0L;
+    public static class Command {
+    }
 
-	{
-		receive();
-	}
+    {
+        receive();
+    }
 
-	private void onCommand(Command c) {
-		messages++;
-		if (messages % 4 == 0) {
-			throw new RuntimeException("Oh no, I got four commands, I can't handle any more");
-		} else {
-			log().info("Got a command " + messages);
-		}
-	}
+    private void onCommand(Command c) {
+        messages++;
+        if (messages % 4 == 0) {
+            throw new RuntimeException("Oh no, I got four commands, I can't handle any more");
+        } else {
+            log().info("Got a command " + messages);
+        }
+    }
 
-	public static Props props() {
-		return Props.create(NonTrustWorthyChild.class);
-	}
+    public static Props props() {
 
-	@Override
-	public Receive createReceive() {
-		return receiveBuilder().match(Command.class, this::onCommand).build();
-	}
+        return Props.create(NonTrustWorthyChild.class);
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder().match(Command.class, this::onCommand).build();
+    }
 
 }
