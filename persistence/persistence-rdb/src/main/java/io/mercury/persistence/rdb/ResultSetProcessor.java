424919e309b25e9c88e3b210e2dcfd204f5a7fdf
yellow013 <wk9988@gmail.com>
2024-01-07 21:21:39 +0900

    update code

--- persistence/persistence-rdb/src/main/java/io/mercury/persistence/rdb/ResultSetProcessor.java
+++ persistence/persistence-rdb/src/main/java/io/mercury/persistence/rdb/ResultSetProcessor.java
@@ -3,24 +3,23 @@ package io.mercury.persistence.sqlite;
 import org.apache.commons.dbutils.RowProcessor;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Map;
 
 @FunctionalInterface
 public interface ResultSetProcessor extends RowProcessor {
 
     @Override
-    default Object[] toArray(ResultSet rs) throws SQLException {
+    default Object[] toArray(ResultSet rs) {
         throw new UnsupportedOperationException("Unsupported toArray(ResultSet) function");
     }
 
     @Override
-    default Map<String, Object> toMap(ResultSet rs) throws SQLException {
+    default Map<String, Object> toMap(ResultSet rs) {
         throw new UnsupportedOperationException("Unsupported toMap(ResultSet) function");
     }
 
     @Override
-    default <T> T toBean(ResultSet rs, Class<? extends T> type) throws SQLException {
+    default <T> T toBean(ResultSet rs, Class<? extends T> type) {
         throw new UnsupportedOperationException("Unsupported toBean(ResultSet, Class) function");
     }
 
